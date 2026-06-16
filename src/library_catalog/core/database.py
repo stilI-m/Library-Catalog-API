02b263ff36d061bb3ff03630eb9a37ca675e6f1a
stilI-m <henrybolton2kzz@gmail.com>
2026-06-16 12:15:45 +0300

    feat: add Alembic migrations and books table (update docker-compose file for even connect)

--- src/library_catalog/core/database.py
+++ src/library_catalog/core/database.py
@@ -9,7 +9,7 @@ from src.library_catalog.core.config import settings
 class Base(DeclarativeBase):
     pass
 
-engine = create_async_engine(settings.database_url_str, pool_size= settings.database_pool_size, echo=settings.debug,)
+engine = create_async_engine(str(settings.database_url), pool_size= settings.database_pool_size, echo=settings.debug,)
 
 async_session = async_sessionmaker(bind=engine,  class_=AsyncSession, expire_on_commit=False,)
 
